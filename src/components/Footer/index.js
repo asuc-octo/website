b8681a66eb774b1665436a4eba4827c7ce7938eb
Michael Li <michael.li1324@gmail.com>
2018-07-11 00:04:01 -0400

    footer styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,30 @@
-/**
- * Created by vaibhav on 31/3/18
- */
-import React from 'react'
-import config from '../../../meta/config'
+import React from 'react';
+import Link from 'gatsby-link';
+import config from '../../../meta/config';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
+    <footer className='footer is-primary'>
       <div className='container'>
-        <div className='content has-text-centered'>
-          <p>
-            {config.copyright}
-          </p>
+        <div className='content is-medium'>
+          <div className='level'>
+            <div className='level-left'>
+              <div className='level-item'>
+                <p className='footer-copyright'>{config.copyright}</p>
+              </div>
+            </div>
+            <div className='level-right'>
+              <div className='footer-links'>
+                <div className='footer-link'><Link to='/'>Contact</Link></div>
+                <div className='footer-link'><Link to='/'>Facebook</Link></div>
+                <div className='footer-link'><Link to='/'>Recruitment</Link></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
